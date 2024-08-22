79bda536ebac87d8ebeb3cd81f1760b9680058f3
BrayanStevenRamirezDiaz <brayan.ramirez@claro.com.co>
2024-08-22 16:07:06 -0500

    Carita feliz

--- archivoPython.py
+++ archivoPython.py
@@ -1,14 +1,35 @@
-print("hola Mundo")
-print("Este es un mensaje agregado")
+import time
+import os
 
+def clear_screen():
+    # Esto limpia la pantalla, funciona en Windows y Unix
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+def print_smiley(position):
+    # Imprime la carita feliz en una posición específica
+    spaces = ' ' * position
+    print(f"{spaces}:-)\n")
 
-x = 2
-y = 3
+def main():
+    # Tamaño de la pantalla
+    width = 40
 
-z = x + y
+    try:
+        while True:
+            for position in range(width):
+                clear_screen()
+                print_smiley(position)
+                time.sleep(0.1)  # Ajusta el tiempo de espera para controlar la velocidad
 
-print("Este es el resultado de la suma de x + y: " , z)
+            for position in range(width - 1, -1, -1):
+                clear_screen()
+                print_smiley(position)
+                time.sleep(0.1)  # Ajusta el tiempo de espera para controlar la velocidad
 
+    except KeyboardInterrupt:
+        # Permite detener el programa con Ctrl+C
+        clear_screen()
+        print("¡Hasta luego!")
 
-print("Este es el nuevo cambio que va a generarce")
+if __name__ == "__main__":
+    main()
